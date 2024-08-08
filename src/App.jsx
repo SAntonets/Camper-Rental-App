2d8e063e8f59f1ce99b117c2e29634c2ef8b8eed
Serhii Antonets <antonets.serhii.ua@gmail.com>
2024-08-08 16:03:42 +0300

    add lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,25 @@
+import React, { Suspense, lazy } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import './App.css';
 
-import { Navigate, Route, Routes } from 'react-router'
-import './App.css'
-import CatalogPage from './pages/CatalogPage/CatalogPage'
-import FavoritesPage from './pages/FavoritesPage/FavoritesPage'
-import HomePage from './pages/HomePages/HomePage'
+// Лейзі лоадінг компонентів
+const HomePage = lazy(() => import('./pages/HomePages/HomePage'));
+const CatalogPage = lazy(() => import('./pages/CatalogPage/CatalogPage'));
+const FavoritesPage = lazy(() => import('./pages/FavoritesPage/FavoritesPage'));
 
 function App() {
-
-
   return (
-    <>
-      <Routes>
-          <Route path="/" element={<HomePage />} />
+    <Router>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route index element={<HomePage />} />
           <Route path="/catalog" element={<CatalogPage />} />
           <Route path="/favorites" element={<FavoritesPage />} />
-          <Route path="*" element={<Navigate to={'/'} />} />
-      </Routes>
-    </>
-  )
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </Suspense>
+    </Router>
+  );
 }
 
-export default App
+export default App;
