d43bc3894337d482a426804ac978e58189f7586a
Serhii Antonets <antonets.serhii.ua@gmail.com>
2024-08-08 15:58:35 +0300

    add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 
+import { Navigate, Route, Routes } from 'react-router'
 import './App.css'
+import CatalogPage from './pages/CatalogPage/CatalogPage'
+import FavoritesPage from './pages/FavoritesPage/FavoritesPage'
+import HomePage from './pages/HomePages/HomePage'
 
 function App() {
 
 
   return (
     <>
-
+      <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/catalog" element={<CatalogPage />} />
+          <Route path="/favorites" element={<FavoritesPage />} />
+          <Route path="*" element={<Navigate to={'/'} />} />
+      </Routes>
     </>
   )
 }
